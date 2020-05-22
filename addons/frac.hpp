19525b048b3599932bcd9b5ac02051239f54dced
Onkar Ingale <ingale2273@gmail.com>
2020-05-22 22:05:17 +0530

    Addons for matrixE

---
+++ addons/frac.hpp
@@ -0,0 +1,7 @@
+#include<stdint.h>
+#ifndef FRAC_H
+#define FRAC_H
+void frac(double f1);
+void rat_approx(double f, int64_t md, int64_t *num, int64_t *denom);
+
+#endif // __FRAC_H
---
+++ addons/frac.main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include"frac.hpp"
+#include"frac.cpp"
+void main ()
+{
+    float a= 1.5;
+    frac(a);
+
+}
